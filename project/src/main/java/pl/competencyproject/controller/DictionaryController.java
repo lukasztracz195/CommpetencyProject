e3057f7fd76cdaab39f882c13e9c6d898bf83d3c
Piter <piterpoland@interia.pl>
2018-12-06 20:20:49 +0100

    Add FileChooser to DictionaryController

--- project/src/main/java/pl/competencyproject/controller/DictionaryController.java
+++ project/src/main/java/pl/competencyproject/controller/DictionaryController.java
@@ -3,8 +3,11 @@ package pl.competencyproject.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DictionaryController extends AbstractController implements Initializable {
@@ -21,7 +24,15 @@ public class DictionaryController extends AbstractController implements Initiali
 
     @FXML
     public void addDictionary(){
+        FileChooser fileChooser=new FileChooser();
+        fileChooser.setInitialDirectory(new File("C:\\Users"));
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("CSV Files","*.csv"));
 
+        List<File> selectedFiles=fileChooser.showOpenMultipleDialog(null);
+        if(selectedFiles!=null){
+            for(int i=0;i<selectedFiles.size();i++)
+                selectedFiles.get(i).getAbsolutePath();
+        }
     }
     @FXML
     public void back() {
