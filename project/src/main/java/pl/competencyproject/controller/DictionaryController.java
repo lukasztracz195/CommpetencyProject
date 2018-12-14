508fc419dd14fc5975904aa3de246a2c0b252385
Piter <piterpoland@interia.pl>
2018-12-14 17:13:43 +0100

    Add listener
    add listener to add new dictionary button, changes to connection with filechooser

--- project/src/main/java/pl/competencyproject/controller/DictionaryController.java
+++ project/src/main/java/pl/competencyproject/controller/DictionaryController.java
@@ -4,11 +4,15 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
+import pl.competencyproject.model.csv.CSVReader;
+import pl.competencyproject.model.enums.TypeOfUsedDatabase;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -22,6 +26,8 @@ public class DictionaryController extends AbstractController implements Initiali
     @FXML
     private ChoiceBox nameOfCategoryChoiceBox;
     @FXML
+    private Button addNewDictionaryButton;
+    @FXML
     private Label clockLabel;
     @FXML
     private Label dateLabel;
@@ -34,15 +40,21 @@ public class DictionaryController extends AbstractController implements Initiali
     }
 
     @FXML
-    public void addDictionary(){
+    public void addDictionary() throws FileNotFoundException {
+        CSVReader csvReader=CSVReader.getInstance(TypeOfUsedDatabase.OnlineOrginalDatabase);
+        csvReader.chooseLevel(nameOfLevelChoiceBox.getSelectionModel().toString(),nameOfCategoryChoiceBox.getSelectionModel().toString());
+
         FileChooser fileChooser=new FileChooser();
         fileChooser.setInitialDirectory(new File("C:\\Users"));
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("csv Files","*.csv"));
 
         List<File> selectedFiles=fileChooser.showOpenMultipleDialog(null);
         if(selectedFiles!=null){
-            for(int i=0;i<selectedFiles.size();i++)
+            for(int i=0;i<selectedFiles.size();i++) {
                 selectedFiles.get(i).getAbsolutePath();
+                csvReader.chooseCSV(selectedFiles.get(i));
+            }
+
         }
     }
     @FXML
@@ -52,6 +64,7 @@ public class DictionaryController extends AbstractController implements Initiali
         nameOfLevelChoiceBox.setDisable(true);
         nameOfCategoryChoiceBox.setValue(null);
         nameOfCategoryChoiceBox.setDisable(true);
+        addNewDictionaryButton.setDisable(true);
     }
     @FXML
     public void back() {
@@ -74,5 +87,6 @@ public class DictionaryController extends AbstractController implements Initiali
     private void enableChoiceBoxes(){
         typeOfDictionaryChoiceBox.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue)->nameOfLevelChoiceBox.setDisable(false));
         nameOfLevelChoiceBox.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue)->nameOfCategoryChoiceBox.setDisable(false));
+        nameOfCategoryChoiceBox.getSelectionModel().selectedItemProperty().addListener((v,oldValue,newValue)->addNewDictionaryButton.setDisable(false));
     }
 }
