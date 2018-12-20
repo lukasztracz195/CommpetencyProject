7891d8b5ae41ac3c75f0cd804cb8b9c1dc7568a2
209489 <lukasztracz195@gmail.com>
2018-12-20 20:43:53 +0100

    Make project as runnable

--- project/src/main/java/pl/competencyproject/model/mechanicsOfQuestion/Teacher.java
+++ project/src/main/java/pl/competencyproject/model/mechanicsOfQuestion/Teacher.java
@@ -1,10 +1,7 @@
+/*
 package pl.competencyproject.model.mechanicsOfQuestion;
 
-import pl.competencyproject.model.dao.SessionLogon;
-import pl.competencyproject.model.dao.classes.ManageDictionaryWords;
-import pl.competencyproject.model.dao.classes.ManageWordsENG;
-import pl.competencyproject.model.dao.classes.ManageWordsPL;
-import pl.competencyproject.model.enums.TypeDictionaryDownloaded;
+import pl.competencyproject.model.enums.TypeOfDictionaryDownloaded;
 import pl.competencyproject.model.enums.TypeOfDictionaryLanguage;
 import pl.competencyproject.model.enums.TypeOfUsedDatabase;
 
@@ -23,7 +20,7 @@ public class Teacher {
             System.out.println("Wybierz poziom");
             Scanner in = new Scanner(System.in);
             Integer level = in.nextInt();
-            map.loadDictionary(level, TypeDictionaryDownloaded.DictionaryOfWords, TypeOfDictionaryLanguage.PLtoENG, TypeOfUsedDatabase.OnlineOrginalDatabase);
+            map.loadDictionary(level, TypeOfDictionaryDownloaded.DictionaryOfWords, TypeOfDictionaryLanguage.PLtoENG, TypeOfUsedDatabase.OnlineOrginalDatabase);
             if(level!=null)
             {
                 currentMapQuestion = map.getRandTenMap();
@@ -73,7 +70,7 @@ public class Teacher {
         /*System.out.println("Chcesz kontynuować naukę tego poziomu? ");
         Scanner in = new Scanner(System.in);
         String answer = in.nextLine();
-        if(answer=="tak")*/
-
-    }
+        if(answer=="tak")
 }
+}
+*/
